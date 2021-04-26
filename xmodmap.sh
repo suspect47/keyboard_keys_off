0ac2cf6b585d04e5af5a91d8220d7cbe696a7830
Ilya Igonin <47140422+suspect47@users.noreply.github.com>
2021-04-26 16:30:36 +0300

    Update xmodmap.sh

--- xmodmap.sh
+++ xmodmap.sh
@@ -1,39 +1,33 @@
 #!/bin/bash
-
 set -e
-
-exec > /home/user/inotify.log
-
+#exec > /home/user/inotify.log
 function xmm {
-
 xmodmap -e 'remove mod4 = Super_L'
 xmodmap -e 'add mod3 = Super_L'
-xmodmap -e "keycode 9 = Super_L"
-xmodmap -e "keycode 105 = Super_L"
-xmodmap -e "keycode 37 = Super_L"
-xmodmap -e "keycode 62 = Super_L"
-xmodmap -e "keycode 67 = Super_L"
-xmodmap -e "keycode 68 = Super_L"
-xmodmap -e "keycode 69 = Super_L"
-xmodmap -e "keycode 70 = Super_L"
-xmodmap -e "keycode 72 = Super_L"
-xmodmap -e "keycode 73 = Super_L"
-xmodmap -e "keycode 74 = Super_L"
-xmodmap -e "keycode 75 = Super_L"
-xmodmap -e "keycode 76 = Super_L"
-xmodmap -e "keycode 95 = Super_L"
-xmodmap -e "keycode 96 = Super_L"
+xmodmap -e "keycode 9 = Shift_L"
+xmodmap -e "keycode 105 = Shift_L"
+xmodmap -e "keycode 37 = Shift_L"
+xmodmap -e "keycode 62 = Shift_L"
+xmodmap -e "keycode 67 = Shift_L"
+xmodmap -e "keycode 68 = Shift_L"
+xmodmap -e "keycode 69 = Shift_L"
+xmodmap -e "keycode 70 = Shift_L"
+xmodmap -e "keycode 71 = Shift_L"
+xmodmap -e "keycode 72 = Shift_L"
+xmodmap -e "keycode 73 = Shift_L"
+xmodmap -e "keycode 74 = Shift_L"
+xmodmap -e "keycode 75 = Shift_L"
+xmodmap -e "keycode 76 = Shift_L"
+xmodmap -e "keycode 95 = Shift_L"
+xmodmap -e "keycode 96 = Shift_L"
 }
-
 setxkbmap -option -option grp_led:scroll,grp:toggle
-
 xmm
-
 inotifywait --event create -m /dev/input |
 while read CHANGED;
 do
 echo "$(date) change event caught; sleeping for 2 seconds and then disabling keys"
-sleep 2
+sleep 1
 echo "$(date) Disabling keys"
 xmm
 echo "$(date) done;"
